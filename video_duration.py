edc50b630f0930b5fd8f99b3c0552ef38f5cea84
Brandon Barton <bcbarton.dw@gmail.com>
2012-03-30 04:32:56 -0700

    Many fixes. Program is now stable.
    lsvd now does the following:
    	-Does not crash when given non-video files!
    	-Works for very large amounts of video durations.
    	-Is now slightly faster.
    	-Cleaned up some string processing code

--- video_duration.py
+++ video_duration.py
@@ -1,15 +1,26 @@
 #3/29/2012
 #Converts a single video files into a summed duration
-import subprocess, re
+#TODO: Add test cases
+import subprocess, re, datetime
 from dateutil.parser import *
 
+#Warning: If ffprobe's output is ever changed, this part might break.
 def getLength(filename):
   result = subprocess.Popen(["ffprobe", filename],
     stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
   return [x for x in result.stdout.readlines() if "Duration" in x]
 
 def getDurationTimestamp(filename):
-	stamp = getLength(filename)[0]
+	stamp = getLength(filename)
+
+	#Check for case of not a video file
+	if not stamp:
+		return datetime.timedelta() #Return empty timedelta for non-video files
+	else:
+		stamp = stamp[0]
+
 	trimmedStamp = re.findall(r'\d\d:\d\d:\d\d',stamp)
 	trimmedStamp = trimmedStamp[0]
-	return parse(trimmedStamp) #FIXIT: Make this work with more than 24 hours of video
+	ts = parse(trimmedStamp) #Turns the timestamp string into a datetime object
+	return datetime.timedelta(hours = ts.hour, seconds = ts.second, minutes = ts.minute)
+
